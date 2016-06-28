8292a66c6bab381a0157d0d7995118a064ca9bb9
Ryan Hunter <rymalhunt@gmail.com>
2016-06-28 09:54:10 -0600

    Set up override to add preferences to General Settings page...disabled because not working yet

---
+++ app/overrides/admin/general_settings/edit/add_custom_preferences.rb
@@ -0,0 +1,6 @@
+# Deface::Override.new(
+#   :name          => "add_custom_preferences",
+#   :virtual_path  => "spree/admin/general_settings/edit",
+#   :insert_before => "#preferences[data-hook]",
+#   :partial => 'tshop/admin/general_settings/form_fields',
+# )
